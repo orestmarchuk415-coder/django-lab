3878ab37a814e6c54eea5fe3f6b16d3d0f0c3bb7
orest <orestmarchuk415@gmail.com>
2025-10-18 18:53:05 +0300

    Lab 7: Created app_blog and first page

---
+++ app_blog/views.py
@@ -0,0 +1,9 @@
+
+# Create your views here.
+# app_blog/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
